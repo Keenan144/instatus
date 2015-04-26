45f1b3a198d17d92126feb0364279dcd05af9bbd
Keenan Sturtevant <keenansturtevant@gmail.com>
2015-04-26 11:57:04 -0500

    completed rough MVP, reactive and ready for backend code

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,23 @@
 get '/' do 
   erb :landingpage
 end
+
+get '/news' do 
+  erb :news
+end
+
+get '/forms' do 
+  erb :forms
+end
+
+get '/calendar' do 
+  erb :calendar
+end
+
+get '/chat' do 
+  erb :chat
+end
+
+get '/voicecalls' do 
+  erb :voicecalls
+end
